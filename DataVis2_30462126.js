d6083bc8b9b7f9663947d3623d8310b28342bf54
azzam18 <92562960+azzam18@users.noreply.github.com>
2021-11-22 08:13:34 +0700

    Files for github page

---
+++ DataVis2_30462126.js
@@ -0,0 +1,34 @@
+//Visualisation 1 
+var vg1 = "Vis1.json";
+
+vegaEmbed('#vis1', vg1,{"actions": false}).then(function(result){
+
+}).catch(console.error);
+
+//Visualisation 2 
+var vg2 = "Vis2.json";
+
+vegaEmbed('#vis2', vg2,{"actions": false}).then(function(result){
+
+}).catch(console.error);
+
+//Visualisation 3
+var vg3 = "Vis3.json";
+
+vegaEmbed('#vis3', vg3,{"actions": false}).then(function(result){
+
+}).catch(console.error);
+
+//Visualisation 4
+var vg4 = "Vis4.json";
+
+vegaEmbed('#vis4', vg4,{"actions": false}).then(function(result){
+
+}).catch(console.error);
+
+//Visualisation 5
+var vg5 = "Vis5.json";
+
+vegaEmbed('#vis5', vg5,{"actions": false}).then(function(result){
+
+}).catch(console.error);
